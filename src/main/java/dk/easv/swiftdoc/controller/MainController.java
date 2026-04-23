a80cb9e0b65d66aaf553ce8afb1276413488671e
mirekondro1 <mirekondro@post.cz>
2026-04-23 19:51:49 +0200

    Add main FXML + controller stub

---
+++ src/main/java/dk/easv/swiftdoc/controller/MainController.java
@@ -0,0 +1,21 @@
+package dk.easv.swiftdoc.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    private void initialize() {
+        // TODO: initialize the main view
+    }
+
+    @FXML
+    private void onNewCommand() {
+        // TODO: handle new command
+    }
+
+    @FXML
+    private void onSaveCommand() {
+        // TODO: handle save command
+    }
+}
+
