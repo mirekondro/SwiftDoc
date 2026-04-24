65e44ad6e3508791c5df14e5cf9cdbdea4196f7e
mirekondro1 <mirekondro@post.cz>
2026-04-24 07:20:42 +0200

    Show error dialog on failure

--- src/main/java/dk/easv/swiftdoc/app/HelloApplication.java
+++ src/main/java/dk/easv/swiftdoc/app/HelloApplication.java
@@ -1,19 +1,32 @@
 package dk.easv.swiftdoc.app;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/dk/easv/swiftdoc/view/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/dk/easv/swiftdoc/view/main-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception ex) {
+            showStartupErrorDialog(ex);
+            Platform.exit();
+        }
+    }
+
+    private void showStartupErrorDialog(Exception ex) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to start the application.\n\n" + ex.getMessage(), ButtonType.OK);
+        alert.setTitle("Startup Error");
+        alert.setHeaderText("WebLager could not be started");
+        alert.showAndWait();
     }
 }
